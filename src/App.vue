42d25bddbcc02115abf4118364fcf17924e03654
yq_shmily <yq_cherish@qq.com>
2021-04-30 19:40:54 +0800

    [A] add lottie

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,43 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="animation-style" ref="animation"></div>
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
+import lottie from 'lottie-web';
+// import Trophy from '@/assets/json/bulb.json';
+import manFlowers from '@/assets/json/manFlowers.json';
+// import Trophy from '@/assets/json/Trophy.json';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted() {
+    this.anim = lottie.loadAnimation({
+          container: this.$refs.animation,
+          renderer: 'svg',
+          loop: true,
+          autoplay: true,
+          animationData: manFlowers,//动画json
+        }
+    )
   }
 }
 </script>
 
 <style>
+.animation-style {
+  width: 100%;
+  margin: 0 auto;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Expecting.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>123</div>
+    <div class="animation-style" ref="animation"></div>
+  </div>
+</template>
+
+<script>
+import * as Trophy from '@/assets/json/Trophy.json';
+import lottie from 'lottie-web';
+export default {
+  name: "expecting",
+  mounted () {
+    lottie.loadAnimation({
+          container: this.$refs.animation,
+          renderer: 'svg',
+          loop: true,
+          autoplay: true,
+          animationData: Trophy,//动画json
+        }
+    )
+  }
+}
+</script>
+
+<style scoped>
+.animation-style{
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
+}
+</style>
